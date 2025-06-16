4103ba6cba9a3fb5fb75ebee3aec42774a8e6c94
MayconProg <maycondouglass.mg@gmail.com>
2025-06-16 11:41:19 -0300

    Feat: Add Email Authentication with Clerk

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -2,17 +2,40 @@ import { CustomButton } from "@/components/CustomButton";
 import { InputField } from "@/components/InputField";
 import { OAuth } from "@/components/OAuth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
+import { useSignIn } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
 import { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 
 export default function SignIp() {
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const router = useRouter();
+
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
-  async function onSignInPress() {}
+  async function onSignInPress() {
+    if (!isLoaded) return;
+
+    // Start the sign-in process using the email and password provided
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/(root)/(tabs)/home");
+      } else {
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }
 
   return (
     <ScrollView className="flex-1 bg-white">
